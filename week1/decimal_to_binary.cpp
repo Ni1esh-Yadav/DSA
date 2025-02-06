3c1b3789d95f579fef37b8c2e6912622155d54b3
Ni1esh-Yadav <yadav.nilesh9172@gmail.com>
2025-02-06 15:15:00 +0530

    added binary to decimal code

--- week1/decimal_to_binary.cpp
+++ week1/decimal_to_binary.cpp
@@ -17,24 +17,40 @@ using namespace std;
 //  }
 
 // method2 using bitwise operator
-int decimal_to_binary(int num)
-{
-    int binarynum = 0;
-    int i = 0;
-    while (num > 0)
-    {
-        int digit = (num & 1);
-        binarynum = digit * pow(10, i++) + binarynum;
-        num = num >> 1;
-    }
-    return binarynum;
-}
+// int decimal_to_binary(int num)
+// {
+//     int binarynum = 0;
+//     int i = 0;
+//     while (num > 0)
+//     {
+//         int digit = (num & 1);
+//         binarynum = digit * pow(10, i++) + binarynum;
+//         num = num >> 1;
+//     }
+//     return binarynum;
+// }
+
+// binary to digit conversion
+// int binary_to_decimal(int num)
+// {
+//     int decimal = 0;
+//     int i = 0;
+//     while (num > 0)
+//     {
+//         int bit = num % 10;
+//         decimal = decimal + bit * pow(2, i++);
+//         num /= 10;
+//     }
+//     return decimal;
+// }
 
 int main()
 {
     int num;
     cin >> num;
-    int binary_num = decimal_to_binary(num);
-    cout << binary_num;
+    // int binary_num = decimal_to_binary(num);
+    // cout << binary_num;
+    // int decima_num = binary_to_decimal(num);
+    // cout << decima_num;
     return 0;
 }
